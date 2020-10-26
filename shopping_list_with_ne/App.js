b8ed4902dc04243f0e9c7b9afa30b34e740901c5
Kimmo Savolainen <kimmo.savolainen@myy.haaga-helia.fi>
2020-10-26 02:15:28 +0200

    Koodin stilisointia

--- shopping_list_with_ne/App.js
+++ shopping_list_with_ne/App.js
@@ -1,12 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useEffect, useState } from 'react';
-import { StyleSheet, View, FlatList } from 'react-native';
+import { StyleSheet, View, FlatList, Keyboard } from 'react-native';
 import * as SQLite from 'expo-sqlite';
 import { Header, Input, Button, ListItem, Text, Divider } from 'react-native-elements';
 
 export default function App() {
-
-
   const db = SQLite.openDatabase('shoppingListItemsDb.db');
   const [shopItem, setShopItem] = useState(null);
   const [amount, setAmount] = useState(null);
@@ -19,6 +17,7 @@ export default function App() {
       });
     })
   }
+
   useEffect(() => {
     db.transaction(tx => {
       tx.executeSql('create table if not exists shoppingItem(id integer primary key not null, product text, amount text);')
@@ -32,7 +31,7 @@ export default function App() {
   }
 
   function add() {
-
+    Keyboard.dismiss();
     db.transaction(tx => {
       tx.executeSql('insert into shoppingItem (product, amount) values (?,?);', [shopItem, amount]);
     }, null, () => updateList());
@@ -55,27 +54,46 @@ export default function App() {
         <ListItem.Title>{item.product}</ListItem.Title>
         <ListItem.Subtitle>{item.amount}</ListItem.Subtitle>
       </ListItem.Content>
-      <Button type= "outline" title="bought" buttonStyle={{borderColor:'saddlebrown'}} titleStyle={{color: 'darkmagenta'}} onPress={() => deleteItem(item.id)} />
-
+      <Button
+        type="outline"
+        title="bought"
+        buttonStyle={{ borderColor: 'saddlebrown' }}
+        titleStyle={{ color: 'darkmagenta' }}
+        onPress={() => deleteItem(item.id)}
+      />
     </ListItem>
-
   );
-
-
   return (
     <View>
       <Header
         statusBarProps={{ hidden: true }}
-        centerComponent={{ text: 'Shopping List', style: {color: '#FFF', fontWeight: '600', fontSize: 20} }}
-        containerStyle={{backgroundColor: 'green', paddingVertical:15}}
+        centerComponent={{ text: 'Shopping List', style: { color: '#FFF', fontWeight: '600', fontSize: 20 } }}
+        containerStyle={{ backgroundColor: 'green', paddingVertical: 15 }}
       />
       <View style={{ margin: 10 }}>
         <Text style={{ fontWeight: 'bold' }} >Add some Shopping Items</Text>
-        <Input placeholder="Product" autofocus={true} onChangeText={input => setShopItem(input)} value={shopItem} />
-        <Input placeholder="Amount" onChangeText={input => setAmount(input)} value={amount} />
+        <Input
+          placeholder="Product"
+          autofocus={true}
+          onChangeText={input => setShopItem(input)}
+          value={shopItem}
+        />
+        <Input
+          placeholder="Amount"
+          onChangeText={input => setAmount(input)}
+          value={amount}
+        />
         <View style={styles.btnContainer}>
-          <Button buttonStyle={{ backgroundColor: 'green' }} title="Add" onPress={() => add()}></Button>
-          <Button buttonStyle={{backgroundColor:'saddlebrown'}} title="Clear List" onPress={() => clearItems()}></Button>
+          <Button
+            buttonStyle={{ backgroundColor: 'green' }}
+            title="Add"
+            onPress={() => add()}
+          />
+          <Button
+            buttonStyle={{ backgroundColor: 'saddlebrown' }}
+            title="Clear List"
+            onPress={() => clearItems()}
+          />
         </View>
         <Divider />
         <FlatList
@@ -84,34 +102,24 @@ export default function App() {
           renderItem={renderItem}
         />
       </View>
-
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-
   btnContainer: {
     flexDirection: 'row',
     justifyContent: 'space-around',
     width: 160,
-    marginBottom: 15
-
+    marginBottom: 15,
   },
   textInput: {
     height: 30,
     width: 200,
     borderBottomColor: 'black',
     borderBottomWidth: 1,
-
   },
-
   calcContainer: {
     paddingTop: 400,
   },
-  histContainer: {
-
-  }
-
-
 });
